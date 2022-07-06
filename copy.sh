3b6c02e668c665547657ec00c242ef974eed7472
oditynet <oditynet@gmail.com>
2022-07-06 16:55:24 +0300

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -11,10 +11,10 @@ printf 'Настройка - 1, Копирование данных - 2 '
 read num
 case $num in
 1)
-mkdir /home/DEPO/$1/users
-chown $1:"domain users" /home/DEPO/$1/users
-mkdir /home/DEPO/$1/tmp
-chown $1:"domain users" /home/DEPO/$1/tmp
+mkdir /home/user/$1/users
+chown $1:"domain users" /home/user/$1/users
+mkdir /home/user/$1/tmp
+chown $1:"domain users" /home/user/$1/tmp
 cp -f  pam_mount.conf.xml /etc/security/
 echo -e "\e[32mGood.\e[0m"
 ;;
@@ -22,19 +22,19 @@ echo -e "\e[32mGood.\e[0m"
 2)
 if [[ "$(cat /proc/mounts|grep fclust2|wc -l)" == "1" ]];then
     echo -e "\e[36mOk. Go to copy...\e[0m"
-    mkdir "/home/DEPO/$1/users/Redirected folders"
-    chown $1:"domain users" "/home/DEPO/$1/users/Redirected folders"
-    cd "/home/DEPO/$1/tmp/Redirected folders/"
-    echo -e "$(ls -1 )"|pv -S $(du -sb  "/home/DEPO/$1/tmp/Redirected\ folders/"| awk '{print $1}')|parallel -j 10 rsync --info=progress2  -a '{}' "/home/DEPO/$1/users/Redirected\ folders/"
+    mkdir "/home/user/$1/users/Redirected folders"
+    chown $1:"domain users" "/home/user/$1/users/Redirected folders"
+    cd "/home/user/$1/tmp/Redirected folders/"
+    echo -e "$(ls -1 )"|pv -S $(du -sb  "/home/user/$1/tmp/Redirected\ folders/"| awk '{print $1}')|parallel -j 10 rsync --info=progress2  -a '{}' "/home/DEPO/$1/users/Redirected\ folders/"
     echo -e "\e[36mOk. Copy Downloads...\e[0m"
-    cd "/home/DEPO/$1/Загрузки/"
-    echo -e "$(ls -1 /home/DEPO/$1/Загрузки/)"| parallel -j 10 rsync --info=progress2  -a '{}' "/home/DEPO/$1/users/Redirected\ folders/Downloads/"
+    cd "/home/user/$1/Загрузки/"
+    echo -e "$(ls -1 /home/user/$1/Загрузки/)"| parallel -j 10 rsync --info=progress2  -a '{}' "/home/user/$1/users/Redirected\ folders/Downloads/"
     echo -e "\e[36mOk. Copy Desktop...\e[0m"
-    cp -r -p "/home/DEPO/$1/Desktops/Desktop1/." "/home/DEPO/$1/users/Redirected folders/Desktop"
+    cp -r -p "/home/user/$1/Desktops/Desktop1/." "/home/DEPO/$1/users/Redirected folders/Desktop"
     echo -e "\e[36mOk. Copy Documents...\e[0m"
-    cp -r -p "/home/DEPO/$1/Документы/." "/home/DEPO/$1/users/Redirected folders/Documents"
+    cp -r -p "/home/user/$1/Документы/." "/home/user/$1/users/Redirected folders/Documents"
 
-    cp -f user-dirs.dirs /home/DEPO/$1/.config/
+    cp -f user-dirs.dirs /home/user/$1/.config/
     echo -e "\e[32mGood.\e[0m"
 else
     echo -e "\e[31mfclust2 not mount!\e[0m"
